9281621d6a82d828849bb72cdfa4bab93845b7b2
Prohaddock8 <111456492+Prohaddock8@users.noreply.github.com>
2024-10-14 16:43:09 -0500

    4th

--- server.js
+++ server.js
@@ -2,16 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const notesRouter = require('./routes/notes');
-const calculationHistoryRouter = require('./routes/calculationHistory'); // Import the new route
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 // Middleware
-app.use(cors());
+app.use(cors()); // Enable CORS
 app.use(bodyParser.json());
-app.use('/api/notes', notesRouter); // Existing notes routes
-app.use('/api/calculationHistory', calculationHistoryRouter); // New calculation history routes
+app.use('/api/notes', notesRouter); // Use the notes routes
 
 // Start the server
 app.listen(PORT, () => {
